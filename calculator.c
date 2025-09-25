8ff23eed19883700c7b4f01807e99baca9936efe
giltson1 <josephkabesha@gmail.com>
2025-09-25 18:29:23 -0400

    small fix

--- calculator.c
+++ calculator.c
@@ -5,68 +5,75 @@
 
 int main(int argc, char *argv[])
 {
-   if (argc != 3)
-   {
-      fprintf(stderr, "Usage: calculator <req_read_fd> <reply_write_fd>\n");
-      exit(1);
-   }
-   
-   int req_read_fd = atoi(argv[1]);    // FD to read requests from User
-   int reply_write_fd = atoi(argv[2]); // FD to write replies to User
-   
-   printf("This is the Calculator process (id = %d).\n", getpid());
-   
-   char buffer[100];
-   int num1, num2;
-   char op;
-   int result;
-   
-   while (1)
-   {
-      printf("Calculator Waiting to receive from FD %d\n", req_read_fd);
-      
-      // Read request from User
-      int bytes_read = read(req_read_fd, buffer, sizeof(buffer));
-      if (bytes_read <= 0)
-      {
-         break; // Exit when pipe is closed
-      }
-      
-      // Parse the request
-      sscanf(buffer, "%d %c %d", &num1, &op, &num2);
-      
-      printf("    Calculator received the following: %s from FD %d\n", buffer, req_read_fd);
-      
+  if (argc != 3)
+  {
+    fprintf(stderr, "Usage: calculator <req_read_fd> <reply_write_fd>\n");
+    exit(1);
+  }
+
+  int req_read_fd = atoi(argv[1]);    // FD to read requests from User
+  int reply_write_fd = atoi(argv[2]); // FD to write replies to User
+
+  printf("This is the Calculator process (id = %d).\n", getpid());
+
+  char buffer[100];
+  int num1, num2;
+  char op;
+  int result;
+
+  while (1)
+  {
+    printf("Calculator Waiting to receive from FD %d\n", req_read_fd);
+
+    // Read request from User
+    int bytes_read = read(req_read_fd, buffer, sizeof(buffer));
+    if (bytes_read <= 0)
+    {
+      break; // Exit when pipe is closed
+    }
+    buffer[bytes_read] = '\0'; // Null-terminate the buffer
+
+    // Parse the request
+    int parsed = sscanf(buffer, "%d %c %d", &num1, &op, &num2);
+    if (parsed != 3)
+    {
+      result = -1; // Error: invalid format
+    }
+    else
+    {
       // Perform calculation
       switch (op)
       {
-         case '+':
-            result = num1 + num2;
-            break;
-         case '-':
-            result = num1 - num2;
-            break;
-         case '*':
-            result = num1 * num2;
-            break;
-         case '/':
-            if (num2 != 0)
-               result = num1 / num2;
-            else
-               result = 0; // Handle division by zero
-            break;
-         default:
-            result = 0; // Invalid operation
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 != 0)
+          result = num1 / num2;
+        else
+          result = -1; // Error: division by zero
+        break;
+      default:
+        result = -1; // Error: invalid operation
       }
-      
-      printf("Calculator sent the following to the User: %d on FD %d\n", result, reply_write_fd);
-      
-      // Send result back to User
-      write(reply_write_fd, &result, sizeof(int));
-   }
-   
-   close(req_read_fd);
-   close(reply_write_fd);
-   
-   return 0;
+    }
+
+    printf("    Calculator received the following: %s from FD %d\n", buffer, req_read_fd);
+
+    printf("Calculator sent the following to the User: %d on FD %d\n", result, reply_write_fd);
+
+    // Send result back to User
+    write(reply_write_fd, &result, sizeof(int));
+  }
+
+  close(req_read_fd);
+  close(reply_write_fd);
+
+  return 0;
 }
--- user.c
+++ user.c
@@ -23,28 +23,26 @@ int main(int argc, char *argv[])
    char buffer[100];
    int result;
    
-   while (1)
+   // Read input from user
+   if (scanf("%d %c %d", &num1, &op, &num2) != 3)
    {
-      // Read input from user
-      if (scanf("%d %c %d", &num1, &op, &num2) != 3)
-      {
-         // Stops if scanning goes wrong or stops
-         break;
-      }
-      
-      // Format the request
-      sprintf(buffer, "%d %c %d", num1, op, num2);
-      
-      printf("    User Sending the following: %s on FD %d\n", buffer, req_write_fd);
-      
-      // Send request to Calculator
-      write(req_write_fd, buffer, strlen(buffer) + 1);
-      
-      // Read result from Calculator
-      read(reply_read_fd, &result, sizeof(int));
-      
-      printf("User Received the following result: %d from FD %d\n\n", result, reply_read_fd);
+      // Stops if scanning goes wrong or stops
+      fprintf(stderr, "Invalid input format\n");
+      exit(1);
    }
+
+   // Format the request
+   sprintf(buffer, "%d %c %d", num1, op, num2);
+
+   printf("    User Sending the following: %s on FD %d\n", buffer, req_write_fd);
+
+   // Send request to Calculator
+   write(req_write_fd, buffer, strlen(buffer) + 1);
+
+   // Read result from Calculator
+   read(reply_read_fd, &result, sizeof(int));
+
+   printf("User Received the following result: %d from FD %d\n\n", result, reply_read_fd);
    
    close(req_write_fd);
    close(reply_read_fd);
